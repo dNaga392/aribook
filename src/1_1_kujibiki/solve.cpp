9da0dab686f047c24c68588f63a07962cd68d518
nagayama <nagayama@ipl.co.jp>
2017-01-27 01:17:22 +0900

    :sparkles: #4 solved a probrem 1-1 kujibiki by cpp

--- src/1_1_kujibiki/solve.cpp
+++ src/1_1_kujibiki/solve.cpp
@@ -18,9 +18,49 @@
 #include <iostream>
 
 
+namespace
+{
+	bool is_valid_inputs(int n, int m, const std::vector<int> &k)
+	{
+		if (n < 1 || 50 < n) { return false; }
+		if (m < 1 || 100000000 < m) { return false; }
+		if ((int)k.size() != n) { return false; }
+        for (int ki : k)
+        {
+            if (ki < 0 || 100000000 < ki) { return false; }
+        }
+		return true;
+	}
+}
+
+bool is_winnable_kujibiki_bruteforce(int n, int m, const std::vector<int> &k)
+{
+	if (is_valid_inputs(n, m, k) == false)
+	{
+		return false;
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			for (int p = 0; p < n; ++p)
+			{
+				for (int q = 0; q < n; ++q)
+				{
+					if (k[i] + k[j] + k[p] + k[q] == m)
+					{
+						return true;
+					}
+				}
+			}
+		}
+	}
+	return false;
+}
+
 bool is_winnable_kujibiki(int n, int m, const std::vector<int> &k)
 {
-	return false;
+	return is_winnable_kujibiki_bruteforce(n, m, k);
 }
 
 void show_winnable_kujibiki(int n, int m, const std::vector<int> &k)
